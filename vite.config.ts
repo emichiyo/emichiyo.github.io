cc9ffa86efa110fe095fff043397adc79ace0e72
mjalen <ajalenboi@gmail.com>
2023-04-21 00:33:09 -0500

    Testing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router"
+      ]
+    }),
+    Components({
+      extensions: ["vue"],
+      dirs: ["src/components"],
+      deep: false
+    }),
+  ],
+  resolve: {
+    '@': resolve(__dirname, './src'),
+  }
 });
